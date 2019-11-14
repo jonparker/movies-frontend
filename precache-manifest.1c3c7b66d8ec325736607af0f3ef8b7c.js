dda9808b2441c8ee705aa4bb82c198d94075b039
Jonathan Parker <jonathanparkeremail@gmail.com>
2019-11-14 22:38:27 +1100

    Updates

---
+++ precache-manifest.1c3c7b66d8ec325736607af0f3ef8b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0fc015532ebb37395d5f59d96b5a9a",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "9783a5d877cf1a013750",
+    "url": "/movies-app/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "4d4ac518af38b160692d",
+    "url": "/movies-app/static/js/2.185f1efd.chunk.js"
+  },
+  {
+    "revision": "9783a5d877cf1a013750",
+    "url": "/movies-app/static/js/main.71dae695.chunk.js"
+  },
+  {
+    "revision": "1ed7de5dffc25e77f8c7",
+    "url": "/movies-app/static/js/runtime-main.1e907196.js"
+  }
+]);
