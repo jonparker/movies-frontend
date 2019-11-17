0afb4265fb8207db86a3eeccde9c971bc70b6978
Jonathan Parker <jonathanparkeremail@gmail.com>
2019-11-17 12:42:19 +1100

    Moved backend and frontend together

---
+++ movies-backend/src/MovieSidecar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieSidecar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"{ id: ""cw0076759"", title: ""Star Wars: Episode IV - A New Hope""}");
+        }
+    }
+}
