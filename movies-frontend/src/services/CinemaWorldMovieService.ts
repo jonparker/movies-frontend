7e2668e09acfde59880f5c537df0d1eb432c7405
Jonathan Parker <jonathanparkeremail@gmail.com>
2021-05-11 23:55:57 +1000

    Lint fix

--- movies-frontend/src/services/CinemaWorldMovieService.ts
+++ movies-frontend/src/services/CinemaWorldMovieService.ts
@@ -1,5 +1,5 @@
 import IMovieProps from '../IMovieProps';
-import axios from 'axios';
+//import axios from 'axios';
 
 export default class CinemaWorldMovieService {
     GetMovies() : Array<IMovieProps>
