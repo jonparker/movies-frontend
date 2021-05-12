c174347d9045cc799e1647b659a21607205dedfa
Jonathan Parker <jonathanparkeremail@gmail.com>
2021-05-12 17:11:11 +1000

    Remove useEffect deps

--- movies-frontend/src/MovieAppBar.tsx
+++ movies-frontend/src/MovieAppBar.tsx
@@ -64,7 +64,7 @@ export default function SimpleTabs() {
       setFilmWorldMovies(filmWorldMoviesService.GetMovies());
     };
     fetchAll();
-  }, []);
+  });
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
